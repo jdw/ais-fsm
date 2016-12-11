ee2a5e4e614c085820be0ed782f455d30933bf25
Johannes D. Wirde <johannes.wirde@gmail.com>
2016-12-11 21:00:36 +0100

    Pre rename check in

--- src/org/jdw/ais/fsm/State.java
+++ src/org/jdw/ais/fsm/State.java
@@ -12,7 +12,7 @@ import java.util.HashMap;
  * @author JDW
  */
 public class State {
-    private HashMap<String, String> inputs;
+    private HashMap<String, Signal.value> inputs;
     Integer nextStateId;
     Integer id;
 
@@ -21,11 +21,35 @@ public class State {
         this.inputs = new HashMap<>();
     }
     
-    public void addInput(String name, String signal) {
-        inputs.put(name, signal);
+    public void addInput(String in_signal, Signal.value in_value) {
+        inputs.put(in_signal, in_value); 
     }
     
     public void setNextState(int in_nextState) {
         this.nextStateId = in_nextState;
+    }
+    
+    public void update(final HashMap<String, Signal.value> in_signals) {
+        Boolean toNextState = true;
+        
+        for  (String signalName: inputs.keySet()) {
+            if (!toNextState) break;
+            Signal.value signalValue = inputs.get(signalName);
+            if (signalValue.equals(Signal.value.DC)) continue;
+            
+            toNextState = signalValue.equals(in_signals.get(signalName));
+        }
+    }
+    
+    public String toString() {
+        String ret = "" + id;
+        
+        for  (String signalName: inputs.keySet()) {
+            ret  += "," + inputs.get(signalName);
+        }
+        
+        ret += "," + nextStateId;
+        
+        return ret;
     }
 }
